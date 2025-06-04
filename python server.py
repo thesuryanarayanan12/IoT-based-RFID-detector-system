84bc8f09c0826b6723189282f23f3db72fc85079
thesuryanarayanan12 <suryanarayananbose@gmail.com>
2025-06-04 19:25:03 +0100

    Create python server.py

---
+++ python server.py
@@ -0,0 +1,11 @@
+Connecting to MQTT Broker...
+Successfully connected to MQTT Broker and subscribed to topic 'rfid/access'
+
+[LOG] RFID Tag Detected: 0A 7B 1C 9D
+[INFO] Access GRANTED for Alice (Tag ID: 0A 7B 1C 9D)
+
+[LOG] RFID Tag Detected: 55 C1 0F E3
+[WARNING] Access DENIED for unknown Tag ID: 55 C1 0F E3
+
+[LOG] RFID Tag Detected: DE C0 AD 1F
+[INFO] Access GRANTED for Charlie (Tag ID: DE C
